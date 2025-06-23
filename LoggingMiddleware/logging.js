317711364ef044b1a505d4e3e2c7f99a69c021d6
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-06-23 14:02:25 +0530

    Logging Function Done

--- LoggingMiddleware/logging.js
+++ LoggingMiddleware/logging.js
@@ -1,5 +1,37 @@
+const fs = require("fs")
+const dotenv = require("dotenv");
+dotenv.config()
 
+const LOG_URL = process.env.LOG_API;
+const TOKEN = process.env.TOKEN;
 
-const logging = (req, res) => {
+const logging = async (stack = "backend", level, package, message) => {
 
+    const BODY = {
+        stack: stack,
+        level: level,
+        package: package,
+        message: message
+    }
+    // console.log(TOKEN);
+    const response = await fetch(LOG_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${TOKEN}`
+        },
+        body: JSON.stringify(BODY)
+    })
+
+    
+    console.log(response);
+    // for (check successfully responce 
+    const data = response + "\n";
+    fs.appendFile('logs.txt', data, (err) => {
+        if(err){
+            console.error("error in store txt file: ", err.message);
+        }
+    })
 }
+
+module.exports = { logging };
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config()
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const { logging } = require("./LoggingMiddleware/logging")
+
+const PORT = process.env.PORT;
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json())
+
+// text logging function
+app.post('/test-logging', (req, res) => {
+    console.log(req.body);
+    const {stack, level, package, message } = req.body;
+   logging(stack, level, package, message);
+   res.send("Compleated");
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server is Running on ${PORT}`);
+})
